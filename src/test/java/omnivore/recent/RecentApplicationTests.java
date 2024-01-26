329493db2ea05ee1308eaa018d9f704e7c446f48
Brilly-Bohyun <bohyun564@swu.ac.kr>
2024-01-26 16:34:36 +0900

    feat: 프로젝트 초기 설정
    Related to: #1

---
+++ src/test/java/omnivore/recent/RecentApplicationTests.java
@@ -0,0 +1,13 @@
+package omnivore.recent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
