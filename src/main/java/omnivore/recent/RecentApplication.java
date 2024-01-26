bdeedbffb89c9cddcf61d4d1a6debafba2b4ace5
Brilly-Bohyun <bohyun564@swu.ac.kr>
2024-01-26 16:38:48 +0900

    feat: 프로젝트 초기 설정
    Related to: #1

---
+++ src/main/java/omnivore/recent/RecentApplication.java
@@ -0,0 +1,15 @@
+package omnivore.recent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class RecentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecentApplication.class, args);
+	}
+
+}
